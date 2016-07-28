8878135c23ebafea7c55bee087fbb81d56cbca35
Japan Parikh <parikh.japan30@gmail.com>
2016-07-28 15:48:27 -0700

    Add homepage blueprint

---
+++ vagrant/givel/app/home/controller.py
@@ -0,0 +1,12 @@
+from flask import Blueprint
+from flask_restful import Resource, Api
+from app import app
+
+hw_bp = Blueprint('home', __name__)
+api = Api(hw_bp)
+
+class HelloWorld(Resource):
+      def get(self):
+       return {'Hello': 'HelloWorld'}
+
+api.add_resource(HelloWorld, '/')
