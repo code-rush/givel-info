9e1c360c983604dbccedee5967bcf2d3d0d9be34
Japan Parikh <parikh.japan30@gmail.com>
2016-07-28 13:04:53 -0700

    Indentation changes according to PEP8

--- vagrant/givel/app.py
+++ vagrant/givel/app.py
@@ -15,32 +15,54 @@ app.config['DYNAMO_LOCAL_PORT'] = 8000
 
 dynamodb = boto3.resource('dynamodb')
 
-user_table = dynamodb.create_table(
-	TableName='users',
-	KeySchema=[
-		{
-		'AttributeName': 'email',
-		'KeyType': 'HASH'
-		}
-	],
-	AttributeDefinitions=[
-		{
-			'AttributeName': 'email',
-			'AttributeType': 'S'
-		}
-	],
-	ProvisionedThroughput={
-		'ReadCapacityUnits': 5,
-		'WriteCapacityUnits': 5
-	}
+users = dynamodb.create_table(
+    TableName='users',
+	   KeySchema=[
+        {
+            'AttributeName': 'email',
+		          'KeyType': 'HASH'
+		      }
+	   ],
+  	 AttributeDefinitions=[
+        {
+  			       'AttributeName': 'email',
+  			       'AttributeType': 'S'
+  		    }
+  	 ],
+  	 ProvisionedThroughput={
+  	     'ReadCapacityUnits': 5,
+  		    'WriteCapacityUnits': 5
+    }
 )
 
+community = dynamodb.create_table(
+     TableName='community',
+	    KeySchema=[
+        {
+            'AttributeName': 'name',
+            'KeyType': 'HASH'
+        }
+    ],
+    AttributeDefinitions=[
+        {
+            'AttributeName': 'name',
+            'AttributeType': 'S',
+        }
+    ],
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 5,
+        'WriteCapacityUnits': 5
+    }
+)
+
+
 class HelloWorld(Resource):
-	def get(self):
-		return {'Hello': 'HelloWorld'}
+	   def get(self):
+        return {'Hello': 'HelloWorld'}
+
 
 
 api.add_resource(HelloWorld, '/')
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	   app.run(debug=True)
