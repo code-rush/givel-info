12bfa7a1462d22fd4db6cb67c04f2f28be27cf94
Japan Parikh <parikh.japan30@gmail.com>
2016-07-29 16:46:10 -0700

    config settings

--- vagrant/givel/app/config.py
+++ vagrant/givel/app/config.py
@@ -1,7 +1,7 @@
 from app.app import app
 
-app.config['AWS_ACCESS_KEY_ID'] = YOUR_ACCESS_KEY
-app.config['AWS_SECRET_ACCESS_KEY'] = YOUR_SECRET_ACCESS_KEY
+app.config['AWS_ACCESS_KEY_ID'] = aws_access_key_id
+app.config['AWS_SECRET_ACCESS_KEY'] = aws_secret_access_key
 
 # Testing on local database
 app.config['DYNAMO_ENABLE_LOCAL'] = True
