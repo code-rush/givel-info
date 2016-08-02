f41bd4886bc13cab74ad1a7e6bd72bc3fe58f049
Japan Parikh <parikh.japan30@gmail.com>
2016-08-01 17:16:41 -0700

    Changed port for db testing on localhost

--- vagrant/givel/app/config.py
+++ vagrant/givel/app/config.py
@@ -6,4 +6,4 @@ app.config['AWS_SECRET_ACCESS_KEY'] = aws_secret_access_key
 # Testing on local database
 app.config['DYNAMO_ENABLE_LOCAL'] = True
 app.config['DYNAMO_LOCAL_HOST'] = 'localhost'
-app.config['DYNAMO_LOCAL_PORT'] = 8000
+app.config['DYNAMO_LOCAL_PORT'] = 8888
