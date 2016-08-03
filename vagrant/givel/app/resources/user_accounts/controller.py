261ec92dcdc658c5c2e8e2f5d2be3a421e4b8ed1
Japan Parikh <parikh.japan30@gmail.com>
2016-08-03 13:34:57 -0700

    Add api to create and delete user account

--- vagrant/givel/app/resources/user_accounts/controller.py
+++ vagrant/givel/app/resources/user_accounts/controller.py
@@ -1,26 +1,52 @@
 import boto3
 
-from flask import Blueprint
-from flask_restful import Resource, Api 
+from flask import Blueprint, request, json
+from flask_restful import Resource, Api
 from app import app
 
 from werkzeug.security import generate_password_hash, check_password_hash
-from app.models import dynamodb, users
+from werkzeug.exceptions import NotFound, BadRequest
+from app.models import create_users_table
 
 user_account_api_routes = Blueprint('account_api', __name__)
-
 api = Api(user_account_api_routes)
 
 
-class UserAccount(Resource):
-   def post(self):
-       data = request.get_json(force=True)
-       user = dynamodb.Table('users')
-       response = user.put_item(
-           Item={
-               'first_name': data.get('first_name'),
-               'last_name': data.get('last_name'),
-               'email': data.get('email'),
-               'password': generate_password_hash(data.get('password'))
-           })
-       return 'Success'
+dynamodb = boto3.resource('dynamodb')
+
+# Connect to database and create table if not already created else return Table
+try:
+    users = dynamodb.Table('users')
+    print('Connected to users Table')
+except Exception as e:
+    print('Users Table does not exist!')
+    users = create_users_table()
+
+
+class CreateUserAccount(Resource):
+    def post(self):
+        data = request.get_json(force=True)
+        if not data['email'] or not data['first_name'] or not data['last_name'] or not data['password']:
+            raise BadRequest('Provide all details')
+        password = generate_password_hash(data['password'])
+        user = users.put_item(Item={'email': data['email'],
+                                    'first_name': data['first_name'],
+                                    'last_name': data['last_name'],
+                                    'password': password,
+                                    })
+        return user, 201
+            
+
+class UserProfile(Resource):
+    def delete(self, user_email):
+        user = users.get_item(Key={'email': user_email})
+        if not user:
+            raise BadRequest('User does not exist')
+        return users.delete_item(Key={'email': user_email}), 200
+
+
+
+api.add_resource(CreateUserAccount, '/user_accounts')
+api.add_resource(UserProfile, '/user_accounts/<user_email>')
+
+
