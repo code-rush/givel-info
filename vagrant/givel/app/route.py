f209058dc94a8e301469f702641a1320202ef33a
Japan Parikh <parikh.japan30@gmail.com>
2016-08-02 17:01:06 -0700

    Blueprint for user account api routes

--- vagrant/givel/app/route.py
+++ vagrant/givel/app/route.py
@@ -1,4 +1,7 @@
 from app.home.controller import hw_bp
+from app.resources.user_accounts.controller import user_account_api_routes
+
 
 def build_route(app):
    app.register_blueprint(hw_bp)
+   app.register_blueprint(user_account_api_routes, url_prefix='/api/v1')
