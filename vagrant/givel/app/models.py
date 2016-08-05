031f0e8e95a3340ded67debed784fc9260d20cb6
Japan Parikh <parikh.japan30@gmail.com>
2016-08-04 17:06:37 -0700

    Add required community model

--- vagrant/givel/app/models.py
+++ vagrant/givel/app/models.py
@@ -1,4 +1,3 @@
-import datetime
 import boto3
 
 dynamodb = boto3.resource('dynamodb')
@@ -10,7 +9,7 @@ def create_users_table():
            KeySchema=[
                {
                    'AttributeName': 'email',
-                   'KeyType': 'HASH',
+                   'KeyType': 'HASH'
                }
            ],
            AttributeDefinitions=[
@@ -33,51 +32,31 @@ def create_users_table():
         return users_table
 
 
-# from pynamodb.models import Model
-# from pynamodb.attributes import UnicodeAttribute, UnicodeSetAttribute
-# from pynamodb.attributes import NumberAttribute
-# from pynamodb.attributes import UTCDateTimeAttribute
-# from pynamodb.attributes import JSONAttribute
-
-
-# class Users(Model):
-#    """User Profile"""
-#    class Meta:
-#        table_name = 'users'
-#        read_capacity_units = 10
-#        write_capacity_units = 10
-#    email = UnicodeAttribute(hash_key=True)
-#    first_name = UnicodeAttribute()
-#    last_name = UnicodeAttribute()
-#    password = UnicodeAttribute()
-#    user_follwing = UnicodeSetAttribute(null=True)
-#    user_followers = UnicodeSetAttribute(null=True)
-#    communities = UnicodeSetAttribute(null=True)
-#    primary_community = UnicodeAttribute(null=True)
-#    profile_picture = UnicodeAttribute(null=True)
-#    givel_stars = NumberAttribute(default=0)
-
-
-# class Posts(Model):
-#    """Posts Model"""
-#    class Meta:
-#        table_name = 'posts'
-#        read_capacity_units = 1000
-#        write_capacity_units = 10
-#    description = UnicodeAttribute(null=True)
-#    created_time = UTCDateTimeAttribute(default=datetime.datetime.now())
-#    user = UnicodeAttribute()
-#    hearts = NumberAttribute(default=0)
-#    image = UnicodeAttribute(null=True)
-#    video = UnicodeAttribute(null=True)
-#    stars_achieved = NumberAttribute(default=0)
-
-
-# class Communities(Model):
-#    """Community Model"""
-#    class Meta:
-#        table_name = 'communities'
-#        read_capacity_units = 10
-#        write_capacity_units = 10
-#    community_name = UnicodeAttribute(hash_key=True)
-#    community_users = UnicodeSetAttribute()
+def create_community_table():
+    try:
+        communities_table = dynamodb.create_table(
+            TableName='communities',
+            KeySchema=[
+                {
+                    'AttributeName': 'name',
+                    'KeyType': 'HASH'
+                }
+            ],
+            AttributeDefinitions=[
+                {
+                    'AttributeName': 'name',
+                    'AttributeType': 'S'
+                }
+            ],
+            ProvisionedThroughput={
+                'ReadCapacityUnits': 10,
+                'ReadCapacityUnits': 10
+            }
+        )
+    except Exception as e:
+        try:
+            communities_table = dynamodb.Table('communities')
+        except:
+            print('Communities Table does not exist')
+    finally:
+        return communities_table
