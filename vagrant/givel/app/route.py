7a6bae57c5b9e7d916f48b1643d4dbe321350906
Japan Parikh <parikh.japan30@gmail.com>
2016-08-09 14:46:34 -0700

    Build routes for user activity api's

--- vagrant/givel/app/route.py
+++ vagrant/givel/app/route.py
@@ -1,7 +1,9 @@
 from app.home.controller import hw_bp
 from app.resources.user_accounts.controller import user_account_api_routes
+from app.resources.user_activities.controller import user_acitivity_api_routes
 
 
 def build_route(app):
    app.register_blueprint(hw_bp)
-   app.register_blueprint(user_account_api_routes, url_prefix='/api/v1')
+   app.register_blueprint(user_account_api_routes, url_prefix='/api/v1/user_accounts')
+   app.register_blueprint(user_activity_api_routes, url_prefix='/api/v1/users')
