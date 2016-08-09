c11d139a75b778e4ac333167e5747e30dcfd1e36
Japan Parikh <parikh.japan30@gmail.com>
2016-08-09 14:47:10 -0700

    Add api to fetch followers

--- vagrant/givel/app/resources/user_activities/controller.py
+++ vagrant/givel/app/resources/user_activities/controller.py
@@ -6,7 +6,7 @@ from flask import Blueprint, request
 from flask_restful import Api, Resource
 
 
-user_activities_api_routes = Blueprint('activity_api', __name__)
+user_activity_api_routes = Blueprint('activity_api', __name__)
 api = Api(user_activities_api_routes)
 
 db = boto3.client('dynamodb')
@@ -36,9 +36,48 @@ class UserFollowingActivities(Resource):
             user_following = db.update_item(
                         TableName='users',
                         Key={'email': {'S': data['follow_user']}},
-                        UpdateExpression='ADD followers = :followers',
+                        UpdateExpression='ADD followers = :follower',
                         ExpressionAttributeValues={
-                            ':followers': {'SS': [user_email]}
+                            ':follower': {'SS': [user_email]}
                         }
                     )
         return 200
+
+    def delete(self, user_email):
+        """Unfollows a user"""
+        data = request.get_json(force=True)
+        if data['unfollow_user']:
+            user = db.update_item(
+                        TableName='users',
+                        Key={'email': {'S': user_email}},
+                        UpdateExpression='DELETE :user',
+                        ExpressionAttributeValues={
+                            ':user': {'SS':[data['unfollow_user']]}
+                        }
+                    )
+            user_following = db.update_item(
+                        TableName='users',
+                        Key={'email': {'S': data['unfollow_user']}},
+                        UpdateExpression='DELETE :follower',
+                        ExpressionAttributeValues={
+                            ':follower': {'SS':[user_email]}
+                        }
+                    )
+        return 200
+
+
+class UserFollowers(Resource):
+    def get(self, user_email):
+        """Returns list of followers"""
+        user = db.get_item(
+                        TableName='users',
+                        Key={'email': {'S': user_email}}
+                    )
+        return user['Item']['followers'], 200
+
+
+
+api.add_resource(UserFollowingActivities, '/<user_email>/following')
+api.add_resource(UserFollowers, '/<user_email>/followers')
+
+
