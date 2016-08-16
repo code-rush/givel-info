c34c4fc196b98499c07ef6316dd3285560b5aa42
Japan Parikh <parikh.japan30@gmail.com>
2016-08-15 19:03:18 -0700

    Add api to get all communities from database

--- vagrant/givel/app/resources/communities/controller.py
+++ vagrant/givel/app/resources/communities/controller.py
@@ -5,26 +5,18 @@ from flask_restful import Resource, Api
 
 from app.models import create_community_table
 
-dynamodb = boto3.resource('dynamodb')
+db = boto3.client('dynamodb')
 
 try:
     communities = create_community_table()
     print('Communities table is being created')
-finally:
-    communities = dynamodb.Table('communitites')
-    print('Connected to communities')
+except:
+    pass
 
 
-COMMUNITIES = ['Albuquerque', 'Atlanta', 'Austin', 'Baltimore',
-               'Boston', 'Charlotte', 'Chicago', 'Cleveland', 'Columbus', 
-               'Colorado Springs', 'Dallas', 'Denver', 'Detroit', 'El Paso', 
-               'Fort Worth', 'Fresno', 'Houston', 'Indianapolis', 
-               'Jacksonville', 'Kansas City', 'Las Vegas', 'Long Beach', 
-               'Los Angeles', 'Louisville-Jefferson County', 'Mesa', 'Memphis',
-               'Miami', 'Milwaukee', 'Minneapolis', 'Nashville-Davidson', 
-               'New Orleans', 'New York', 'Oakland', 'Oklahoma', 'Omaha', 
-               'Philadelphia', 'Phoenix', 'Portland', 'Raleigh', 'Sacramento',
-               'San Antonio', 'San Diego', 'San Francisco', 'San Jose', 
-               'Seattle', 'Tuscon', 'Tulsa', 'Virginia', 'Washington', 
-               'Wichita']
 
+class Communities(Resource):
+    def get(self):
+        """Returns all communities"""
+        communities = db.scan(TableName='communities')
+        return communities['Items'], 200
