b91509645fd1244d6a1c1e8e5ebea976a1ee5f25
Japan Parikh <parikh.japan30@gmail.com>
2016-09-03 00:23:02 -0700

    add api to get all communities

--- vagrant/givel/app/resources/communities/controller.py
+++ vagrant/givel/app/resources/communities/controller.py
@@ -20,6 +20,40 @@ community_api_routes = Blueprint('community_api', __name__)
 api = Api(community_api_routes)
 
 
+STATES = {
+    'Arizona': 'AZ',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'DC': 'D.C.',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Maryland': 'MD',
+    'Massachusetts': 'MA',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Missouri': 'MO',
+    'Nebraska': 'NE',
+    'Nevada': 'NV',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Pennsylvania': 'PA',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Virginia': 'VA',
+    'Washington': 'WA',
+    'Wisconsin': 'WI',
+}
+
+
 class Communities(Resource):
     def get(self):
         """Returns all communities"""
@@ -27,8 +61,19 @@ class Communities(Resource):
         items = communities['Items']
         results = {}
         for i in items:
-            results[i['city']['S']] = i['state']['S']
+            results[i['city']['S']] = STATES[i['state']['S']]
         return results, 200
+
+# class CommunityPosts(Resource):
+#     def get(self, user_email):
+#         """Returns all posts from users communities"""
+#         user_communities = db.get_item(TableName='users',
+#                                Key={'email': {'S': user_email}},
+#                                ProjectionExpression='home, home_away'
+#                            )
+#         community_posts = db.scan(TableName='posts',
+#                                FilterExpression
+#                            )
 
 
 
