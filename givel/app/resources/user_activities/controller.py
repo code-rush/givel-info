9b5eec867176de87968559f4f1688eb7f93905ac
Japan Parikh <parikh.japan30@gmail.com>
2016-09-14 00:58:04 -0700

    api to follow user

--- givel/app/resources/user_activities/controller.py
+++ givel/app/resources/user_activities/controller.py
@@ -6,37 +6,65 @@ from app import app
 from flask import Blueprint, request
 from flask_restful import Api, Resource
 
+from app.models import create_challenges_table
+from app.models import create_posts_table
+from app.helper import upload_file
 
 user_activity_api_routes = Blueprint('activity_api', __name__)
 api = Api(user_activity_api_routes)
 
+BUCKET_NAME = 'posts'
+
 db = boto3.client('dynamodb')
+s3 = boto3.client('s3')
 
 try: 
-    posts = create_post_table()
+    table_response = db.describe_table(TableName='posts')
+    if table_response['Table']['TableStatus'] == 'ACTIVE':
+        print('Posts Table exists!')
+    else:
+        posts = create_posts_table()
+        print('Posts Table created!')
+except:
+    pass
+
+try:
+    table_response = db.describe_table(TableName='challenges')
+    if table_response['Table']['TableStatus']:
+        print('Challenges Table exists!')
+    else:
+        challenges = create_challenges_table()
+        print('Challenges Table created!')
 except:
     pass
 
 
-class UserFollowingActivities(Resource):
+class UserFollowing(Resource):
     def get(self, user_email):
         """Returns list of users followings"""
-        user = db.get_item(
-                    TableName='users',
-                    Key={'email': {'S': user_email}}
-                )
-        return user['Item']['following'], 200
+        response_message = {}
+        try:
+            user = db.get_item(
+                        TableName='users',
+                        Key={'email': {'S': user_email}}
+                    )
+            return user['Item']['following'], 200
+        except:
+            response_message['message'] = 'You have no followings!'
+            return response_message, 200
+
 
     def put(self, user_email):
         """Adds a user to the following and followers list"""
         data = request.get_json(force=True)
+        follow_user_email = data['follow_user']
         if data['follow_user']:
             user = db.update_item(
                         TableName='users',
                         Key={'email': {'S': user_email}},
                         UpdateExpression='ADD following = :following',
                         ExpressionAttributeValues={
-                            ':following': {'SS': [data['follow_user']]}
+                            ':following': {'SS': [follow_user_email]}
                         }
                     )
             user_following = db.update_item(
@@ -75,55 +103,93 @@ class UserFollowingActivities(Resource):
 class UserFollowers(Resource):
     def get(self, user_email):
         """Returns list of followers"""
-        user = db.get_item(
-                        TableName='users',
-                        Key={'email': {'S': user_email}}
-                    )
-        return user['Item']['followers'], 200
+        response_message = {}
+        try:
+            user = db.get_item(
+                            TableName='users',
+                            Key={'email': {'S': user_email}}
+                        )
+            return user['Item']['followers'], 200
+        except:
+            response_message['message'] = 'You have no followers!'
+            return response_message, 200
 
 
 class UserPosts(Resource):
     def post(self, user_email):
         """Creates User Posts"""
         post_data = request.get_json(force=True)
+        date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         post = db.put_item(TableName='posts',
                            Item={'user_email': {'S': user_email},
-                                'date_time': {'S': post_data['date_time']}
+                                'date_time': {'S': date}
                            }
                       )
-        if post_data['description']:
-            post = db.update_item(TableName='posts',
-                                  Key={'user_email': {'S': user_email},
-                                       'date_time': {'S': post_data['date_time']}
-                                  },
-                                  UpdateExpression='SET description = :d',
-                                  ExpressionAttributeValues={
-                                      ':d': {'S': post_data['description']}
-                                  }
-                              )
-        if post_data['picture']:
-            post = db.update_item(TableName='posts',
-                                  Key={'user_email': {'S': user_email},
-                                       'date_time': {'S': post_data['date_time']}
-                                  },
-                                  UpdateExpression='SET picture = :p',
-                                  ExpressionAttributeValues={
-                                      ':p': {'S': post_data['picture']}
-                                  }
-                              )
-        if post_data['video']:
-            post = db.update_item(TableName='posts',
-                                  Key={'user_email': {'S': user_email},
-                                       'date_time': {'S': post_data['date_time']}
-                                  },
-                                  UpdateExpression='SET video = :v',
-                                  ExpressionAttributeValues={
-                                      ':v': {'S': post_data['video']}
-                                  }
-                              )
+        try:
+            if post_data['description']:
+                post = db.update_item(TableName='posts',
+                                      Key={'user_email': {'S': user_email},
+                                           'date_time': {'S': date}
+                                      },
+                                      UpdateExpression='SET description = :d',
+                                      ExpressionAttributeValues={
+                                          ':d': {'S': post_data['description']}
+                                      }
+                                  )
+        except:
+            try:
+                if post_data['picture']:
+                    picture = request.files['picture_file']
+                    picture_file = upload_file(picture, BUCKET_NAME, user_email)
+                    post = db.update_item(TableName='posts',
+                                          Key={'user_email': {'S': user_email},
+                                               'date_time': {'S': date}
+                                          },
+                                          UpdateExpression='SET picture = :p',
+                                          ExpressionAttributeValues={
+                                              ':p': {'S': picture_file}
+                                          }
+                                      )
+            except:
+                pass
+
+            try:
+                if post_data['video']:
+                    video = request.files['video_file']
+                    video_file = upload_file(video, BUCKET_NAME, user_email)
+                    post = db.update_item(TableName='posts',
+                                          Key={'user_email': {'S': user_email},
+                                               'date_time': {'S': date}
+                                          },
+                                          UpdateExpression='SET video = :v',
+                                          ExpressionAttributeValues={
+                                              ':v': {'S': video_file}
+                                          }
+                                      )
+            except:
+                pass
+        return post, 200
 
 
-api.add_resource(UserFollowingActivities, '/<user_email>/following')
+# class ChallengePosts(Resource):
+#     def post(self, user_email):
+#         """Creates challenges"""
+#         data = request.get_json(force=True)
+#         date = datetime.datetime.strftime("%Y-%m-%d %H:%M:%S")
+#         challenge_post = db.put_item(TableName='challenges',
+#                                 Key={'user_email': {'S': user_email},
+#                                      'date_time': {'S': date}
+#                                 }
+#                             )
+
+#         try:
+#             if data['description']:
+
+
+
+
+api.add_resource(UserFollowing, '/<user_email>/following')
 api.add_resource(UserFollowers, '/<user_email>/followers')
+api.add_resource(UserPosts, '/<user_email>/post')
 
 
