dae2af390f1dce1adb7a282e317e1b557408c9f1
Japan Parikh <parikh.japan30@gmail.com>
2016-09-14 18:23:19 -0700

    helper functions

--- givel/app/helper.py
+++ givel/app/helper.py
@@ -3,7 +3,20 @@ from werkzeug.utils import secure_filename
 
 s3 = boto3.client('s3')
 
-def upload_file(file, bucket, key):
-    filename = secure_filename(file.filename)
-    s3.upload_file(filename, bucket, key)
-    return filename
+def upload_file(file, bucket, key, extensions):
+    if file and allowed_file(file.filename, extensions):
+        filename = str(bucket)+'.s3.amazonaws.com/'+str(key)
+        upload_file = s3.put_object(
+                        Bucket=bucket,
+                        Body=file,
+                        Key=key,
+                        ACL='public-read',
+                        ContentType='image/jpeg'
+                    )
+        return filename
+    else:
+        return None
+
+
+def allowed_file(filename, extensions):
+    return '.' in filename and filename.rsplit('.', 1)[1] in extensions
