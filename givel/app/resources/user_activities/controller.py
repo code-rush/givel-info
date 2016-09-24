2c56062d48235a2d02280d99e5716c21b6bc6f3b
Japan Parikh <parikh.japan30@gmail.com>
2016-09-24 01:00:32 -0700

    Add api to create edit and get challenges

--- givel/app/resources/user_activities/controller.py
+++ givel/app/resources/user_activities/controller.py
@@ -32,17 +32,16 @@ try:
 except:
     pass
 
-
-# try:
-#     try:
-#         table_response = db.describe_table(TableName='challenges')
-#         if table_response['Table']['TableStatus']:
-#             print('Challenges Table exists!')
-#     except:
-#         challenges = create_challenges_table()
-#         print('Challenges Table created!')
-# except:
-#     pass
+try:
+    try:
+        table_response = db.describe_table(TableName='challenges')
+        if table_response['Table']['TableStatus']:
+            print('Challenges Table exists!')
+    except:
+        challenges = create_challenges_table()
+        print('Challenges Table created!')
+except:
+    pass
 
 
 class UserFollowing(Resource):
@@ -327,25 +326,140 @@ class UsersPost(Resource):
         return response, 200
 
 
-# class ChallengePosts(Resource):
-#     def post(self, user_email):
-#         """Creates challenges"""
-#         data = request.get_json(force=True)
-#         date = datetime.datetime.strftime("%Y-%m-%d %H:%M:%S")
-#         challenge_post = db.put_item(TableName='challenges',
-#                                 Key={'user_email': {'S': user_email},
-#                                      'date_time': {'S': date}
-#                                 }
-#                             )
+class ChallengePosts(Resource):
+    def post(self, user_email):
+        """Creates challenges"""
+        response = {}
+        challenge_data = request.get_json(force=True)
+        date_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        date = date_time.rsplit(' ', 1)[0]
+        time = date_time.rsplit(' ', 1)[1]
 
-#         try:
-#             if data['description']:
+        user = db.get_item(TableName='users',
+                        Key={'email': {'S': user_email}
+                        }
+                    )
+        home_community = user['Item']['home']['S']
+
+        if challenge_data.get('description') == None:
+            raise BadRequest('Cannot create an empty challenge!')
+        else:
+            challenge_post = db.put_item(TableName='challenges',
+                                    Item={'challenge_id': {'S': user_email},
+                                          'challenge_key': {'S': date_time},
+                                          'likes': {'N': '0'},
+                                          'value': {'N': '0'},
+                                          'status': {'S': 'ACTIVE'},
+                                          'comments': {'N': '0'},
+                                          'favorites': {'N': '0'},
+                                          'date': {'S': date},
+                                          'time': {'S': time},
+                                          'stars': {'N': '0'},
+                                          'description': {'S': challenge_data['description']}
+                                    }
+                                )
+            if challenge_data.get('location'):
+                challenge_post = db.update_item(TableName='challenges',
+                                      Key={'challenge_id':{'S': user_email},
+                                           'challenge_key': {'S': date_time}
+                                      },
+                                      UpdateExpression='SET challenge_location = :l',
+                                      ExpressionAttributeValues={
+                                          ':l': {'S': challenge_data['location']}
+                                      }
+                                  )
+            else:
+                challenge_post = db.update_item(TableName='challenges',
+                                      Key={'challenge_id': {'S': user_email},
+                                           'challenge_key': {'S': date_time}
+                                      },
+                                      UpdateExpression='SET challenge_location = :l',
+                                      ExpressionAttributeValues={
+                                          ':l': {'S': home_community}
+                                      }
+                                  )
+            response['message'] = 'Challenge successfully created!'
+            return response, 201
+
+    def put(self):
+        """Edit Challenge"""
+        response = {}
+        challenge_data = request.get_json(force=True)
+        if challenge_data.get('challenge_id') == None and challenge_data.get('challenge_key') == None:
+            raise BadRequest('Challenge ID and KEY is required to edit a post')
+        if challenge_data.get('description') != None:
+            try:
+                post = db.update_item(TableName='challenges',
+                                    Key={'challenge_id': {'S': challenge_data['challenge_id']},
+                                         'challenge_key': {'S': challenge_data['challenge_key']}
+                                    },
+                                    UpdateExpression='SET description = :c',
+                                    ExpressionAttributeValues={
+                                        ':c': {'S': challenge_data['description']}
+                                    }
+                                )
+                response['message'] = 'Successfully edited!'
+            except:
+                raise BadRequest('Failed to edit challenge!')
+        else:
+            raise BadRequest('Only challenge description is allowed to edit!')
+        return response, 200
+
+    def get(self, user_email):
+        """Get all the user's challenges"""
+        response = {}
+        try:
+            user_challenges = db.query(TableName='challenges',
+                                Select='ALL_ATTRIBUTES',
+                                Limit=50,
+                                ConsistentRead=True,
+                                KeyConditionExpression='challenge_id = :e',
+                                ExpressionAttributeValues={
+                                    ':e': {'S': user_email}
+                                }
+                            )
+            current_datetime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S').rsplit(' ', 1)
+            current_date = current_datetime[0].rsplit('-',2)
+            current_time = current_datetime[1].rsplit(':',2)
+            for posts in user_challenges['Items']:
+                post_date = posts['date']['S'].rsplit('-',2)
+                post_time = posts['time']['S'].rsplit(':',2)
+                if post_date[0] == current_date[0]:
+                    if post_date[1] == current_date[1]:
+                        if post_date[2] == current_date[2]:
+                            if post_time[0] == current_time[0]:
+                                if post_time[1] == current_time[1]:
+                                    if post_time[2] == current_time[2]:
+                                        posted_time = '0s'
+                                    else:
+                                        posted_time = str(int(current_time[2]) - int(post_time[2])) + 's'
+                                else:
+                                    posted_time = str(int(current_time[1]) - int(post_time[1])) + 'm'
+                            else:
+                                posted_time = str(int(current_time[0]) - int(post_time[0])) + 'hr'
+                        else:
+                            posted_time = str(int(current_date[2]) - int(post_date[2])) + 'd'
+                    else:
+                        posted_time = str(int(current_date[1]) - int(post_date[1])) + 'M'
+                else:
+                    posted_time = str(int(current_date[0]) - int(post_date[0])) + 'yr'
+                # post_time = calculate_post_deltatime(posts['date'])
+                posts['posted_time'] = {}
+                posts['posted_time']['S'] = posted_time + ' ago'
+                del posts['date']
+                del posts['time']
+            response['message'] = 'Successfully fetched users all challenges!'
+            response['result'] = user_challenges['Items']
+        except:
+            response['message'] = 'Failed to fetch users posts!'
+        return response, 200
 
 
 
 
 api.add_resource(UserFollowing, '/<user_email>/following')
 api.add_resource(UserFollowers, '/<user_email>/followers')
-api.add_resource(UsersPost, '/<user_email>/post')
+api.add_resource(UsersPost, '/<user_email>/post', '/post/edit')
+api.add_resource(ChallengePosts, '/<user_email>/challenge', '/challenge/edit')
 
 
