5670c9ef24ec5bd65b79bfaae2788a2dfd2c9ae6
Japan Parikh <parikh.japan30@gmail.com>
2016-10-05 22:52:54 -0700

    Routes - register blueprint for likes

--- givel/app/route.py
+++ givel/app/route.py
@@ -4,6 +4,7 @@ from app.resources.communities.controller import community_api_routes
 from app.resources.users.activities.following.controller import user_following_activity_api_routes
 from app.resources.users.activities.feeds.posts.controller import user_post_activity_api_routes
 from app.resources.users.activities.feeds.challenges.controller import user_challenge_activity_api_routes
+from app.resources.users.activities.feeds.controller import feed_activity_api_routes
 
 def build_route(app):
    app.register_blueprint(hw_bp)
@@ -12,3 +13,4 @@ def build_route(app):
    app.register_blueprint(user_following_activity_api_routes, url_prefix='/api/v1/users')
    app.register_blueprint(user_post_activity_api_routes, url_prefix='/api/v1/users')
    app.register_blueprint(user_challenge_activity_api_routes, url_prefix='/api/v1/users')
+   app.register_blueprint(feed_activity_api_routes, url_prefix='/api/v1/feeds')
