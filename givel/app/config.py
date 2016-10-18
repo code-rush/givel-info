b6d2a8ffc4fcc8a6bcbf91d3deaeedecb04d333a
Japan Parikh <parikh.japan30@gmail.com>
2016-10-18 00:05:42 -0700

    Removed unnecessary files

--- givel/app/config.py
+++
@@ -1,9 +0,0 @@
-from app.app import app
-
-app.config['AWS_ACCESS_KEY_ID'] = aws_access_key_id
-app.config['AWS_SECRET_ACCESS_KEY'] = aws_secret_access_key
-
-# Testing on local database
-app.config['DYNAMO_ENABLE_LOCAL'] = True
-app.config['DYNAMO_LOCAL_HOST'] = 'localhost'
-app.config['DYNAMO_LOCAL_PORT'] = 8888
