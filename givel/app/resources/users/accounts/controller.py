b832ba95827583f821ae71cfa2f9f87d98859d78
Japan Parikh <parikh.japan30@gmail.com>
2016-12-15 01:19:38 -0800

    add api to get the user's profile

--- givel/app/resources/users/accounts/controller.py
+++ givel/app/resources/users/accounts/controller.py
@@ -16,9 +16,17 @@ from app.models import create_users_table, create_notifications_table
 
 from app.helper import upload_file, check_if_community_exists
 from app.helper import update_member_counts, check_if_user_exists
+from app.helper import check_if_user_following_user, get_user_details
+from app.helper import check_if_user_liked, check_if_user_starred
+from app.helper import check_if_user_starred, check_if_taking_off
+from app.helper import check_challenge_state, check_if_user_commented
+
 
 user_account_api_routes = Blueprint('account_api', __name__)
 api = Api(user_account_api_routes)
+
+user_profile_api_routes = Blueprint('profile_api', __name__)
+profile_api = Api(user_profile_api_routes)
 
 
 BUCKET_NAME = 'gprofilepictures'
@@ -485,6 +493,193 @@ class ForgotPassword(Resource):
             return response, 200
 
 
+class GetUsersProfile(Resource):
+    def post(self, user_email):
+        """Returns user's profile"""
+        response = {}
+        data = request.get_json(force=True)
+
+        if data.get('user_id') == None:
+            raise BadRequest('Please provide user_id.')
+        else:
+            user_profile = db.get_item(TableName='users',
+                            Key={'email': {'S':data['user_id']}})
+
+            user_following = check_if_user_following_user(user_email, 
+                                                      data['user_id'])
+
+            profile_details = {}
+
+            if user_profile['Item'] != None:
+                followings = []
+                for following in user_profile['Item']['following']['SS']:
+                    user_name, profile_picture, home = get_user_details(following)
+                    following_following = check_if_user_following_user(user_email,
+                                                                        following)
+                    user_profile['followings'] = {}
+                    user_profile['followings']['id'] = {}
+                    user_profile['followings']['name'] = {}
+                    user_profile['followings']['following'] = {}
+                    user_profile['followings']['profile_picture'] = {}
+                    user_profile['followings']['home_community'] = {}
+                    user_profile['followings']['id']['S'] = following
+                    user_profile['followings']['name']['S'] = user_name
+                    user_profile['followings']['following']['BOOL'] = following_following
+                    user_profile['followings']['profile_picture']['S'] = profile_picture
+                    user_profile['followings']['home_community']['S'] = home
+                    followings.append(user_profile['followings'])
+
+                followers = []
+                for follower in user_profile['Item']['followers']['SS']:
+                    user_name, profile_picture, home = get_user_details(follower)
+                    following_follower = check_if_user_following_user(user_email, 
+                                                                        follower)
+                    user_profile['follower'] = {}
+                    user_profile['follower']['id'] = {}
+                    user_profile['follower']['name'] = {}
+                    user_profile['follower']['following'] = {}
+                    user_profile['follower']['profile_picture'] = {}
+                    user_profile['follower']['home_community'] = {}
+                    user_profile['follower']['id']['S'] = follower
+                    user_profile['follower']['name']['S'] = user_name
+                    user_profile['follower']['following']['BOOL'] = following_follower
+                    user_profile['follower']['profile_picture']['S'] = profile_picture
+                    user_profile['follower']['home_community']['S'] = home
+                    followers.append(user_profile['follower'])
+
+                badges = []
+
+                profile_details['followers'] = {}
+                profile_details['followers']['SS'] = followers
+                profile_details['followings'] = {}
+                profile_details['followings']['SS'] = followings
+                profile_details['badges'] = {}
+                profile_details['badges']['SS'] = badges
+                profile_details['following'] = {}
+                profile_details['following']['BOOL'] = user_following
+
+                response['message'] = 'Request successful!'
+                response['result'] = profile_details
+            else:
+                response['message'] = 'User not found!'
+
+            return response, 200
+            
+
+class GetUsersProfileFeed(Resource):
+    def post(self, user_email):
+        response = {}
+        data = request.get_json(force=True)
+
+        if data.get('user_id') == None:
+            raise BadRequest('Please provide user_id.')
+        else:
+            user_profile = db.get_item(TableName='users',
+                            Key={'email': {'S':data['user_id']}})
+
+            user_following = check_if_user_following_user(user_email, 
+                                                      data['user_id'])
+
+            posts = db.query(TableName='posts',
+                        Select='ALL_ATTRIBUTES',
+                        Limit=50,
+                        KeyConditionExpression='email = :e',
+                        ExpressionAttributeValues={
+                            ':e': {'S': data['user_id']}
+                        }
+                    )
+
+            users_posts = []
+            for post in posts['Items']:
+                user_name, profile_picture, home = get_user_details(data['user_id'])
+                feed_id = post['email']['S'] + '_' + post['creation_time']['S']
+                liked = check_if_user_liked(feed_id, user_email)
+                starred = check_if_user_starred(feed_id, user_email)
+                commented = check_if_user_commented(feed_id, user_email)
+                taking_off = check_if_taking_off(feed_id, 'posts')
+                post['user'] = {}
+                post['user']['name'] = {}
+                post['user']['id'] = {}
+                post['user']['profile_picture'] = {}
+                post['user']['id']['S'] = data['user_id']
+                post['user']['name']['S'] = user_name
+                post['user']['profile_picture']['S'] = profile_picture
+                post['feed'] = {}
+                post['feed']['id'] = {}
+                post['feed']['id']['S'] = post['email']['S']
+                post['feed']['key'] = {}
+                post['feed']['key']['S'] = post['creation_time']['S']
+                post['liked'] = {}
+                post['starred'] = {}
+                post['commented'] = {}
+                post['taking_off'] = {}
+                post['taking_off']['BOOL'] = taking_off
+                post['liked']['BOOL'] = liked
+                post['starred']['BOOL'] = starred
+                post['commented']['BOOL'] = commented
+                del post['email']
+                del post['value']
+                del post['only_to_followers']
+                users_posts.append(post)
+
+            
+            challenges = db.query(TableName='challenges',
+                            Select='ALL_ATTRIBUTES',
+                            Limit=50,
+                            KeyConditionExpression='email = :e',
+                            ExpressionAttributeValues={
+                                ':e': {'S': data['user_id']}
+                            }
+                        )
+
+            users_challenges = []
+            for challenge in challenges['Items']:
+                user_name, profile_picture, home = get_user_details(
+                                            challenge['creator']['S'])
+                feed_id = challenge['email']['S'] + '_' \
+                          + challenge['creation_time']['S']
+                liked = check_if_user_liked(feed_id, user_email)
+                starred = check_if_user_starred(feed_id, user_email)
+                commented = check_if_user_commented(feed_id, user_email)
+                state = check_challenge_state(challenge['email']['S'],
+                                        challenge['creation_time']['S'])
+                taking_off = check_if_taking_off(feed_id, 'challenges')
+                challenge['user'] = {}
+                challenge['user']['name'] = {}
+                challenge['user']['profile_picture'] = {}
+                challenge['user']['id'] = challenge['creator']
+                challenge['user']['name']['S'] = user_name
+                challenge['user']['profile_picture']['S'] = profile_picture
+                challenge['feed'] = {}
+                challenge['feed']['id'] = challenge['email']
+                challenge['feed']['key'] = challenge['creation_time']
+                challenge['state'] = {}
+                challenge['state']['S'] = state
+                challenge['liked'] = {}
+                challenge['starred'] = {}
+                challenge['commented'] = {}
+                challenge['taking_off'] = {}
+                challenge['taking_off']['BOOL'] = taking_off
+                challenge['liked']['BOOL'] = liked
+                challenge['starred']['BOOL'] = starred
+                challenge['commented']['BOOL'] = commented
+                del challenge['email']
+                del challenge['creator']
+                del challenge['value']
+                del challenge['only_to_followers']
+                users_challenges.append(challenge)
+
+            response['message'] = 'Request successful!'
+            response['result'] = {}
+            response['result']['following'] = user_following
+            response['result']['posts'] = {}
+            response['result']['challenges'] = {}
+            response['result']['posts']['SS'] = users_posts
+            response['result']['challenges']['SS'] = users_challenges
+
+            return response, 200
+
+
 
 
 api.add_resource(UserAccount, '/')
@@ -495,4 +690,6 @@ api.add_resource(ChangePassword, '/<user_email>/password')
 api.add_resource(PostOnlyToFollowers, '/settings/post_only_to_followers/<user_email>')
 api.add_resource(GiveStarsToFollowings, '/stars/share/<user_email>')
 api.add_resource(ForgotPassword, '/settings/forgot_password')
+profile_api.add_resource(GetUsersProfile, '/users/<user_email>')
+profile_api.add_resource(GetUsersProfileFeed, '/users/feeds/<user_email>')
 
